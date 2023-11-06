e392a3494a5e3f69e8ee1f7c29c3a59071977402
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-11-06 09:37:46 -0300

    se agregó broadcast al controlador de sendMsg

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
   socket.on('send-msg', (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit('msg-recieve', data.message);
+      socket.broadcast.to(sendUserSocket).emit('msg-recieve', data.message);
     }
   });
 });
