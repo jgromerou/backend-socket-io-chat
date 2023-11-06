c19976e1f4177d5cf58c0e26ec985f8b0cbde492
jgromerou <jgromerouro@gmail.com>
2023-11-05 22:46:09 -0300

    se instaló, se configuró socket para add user y send receive message de un usuario. se terminó con el socket.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { socket } from './sockets/socket.js';
+//import { createServer } from 'http';
+//import { Server } from 'socket.io';
+import socket from 'socket.io';
+
 import morgan from 'morgan';
 import path from 'path';
 import './database/dbConnection';
@@ -13,7 +14,7 @@ import messageRouter from './routes/messagesRoute.js';
 
 dotenv.config();
 const app = express();
-const server = createServer(app);
+//const server = createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -25,13 +26,13 @@ app.use(morgan('dev'));
 app.use(express.static(publicPath));
 
 // IO = esta es la comunicacion del backend
-const io = new Server(server);
+//const io = new Server(server);
 
-io.on('connection', (client) => {
-  socket(client);
-});
+// io.on('connection', (client) => {
+//   socket(client);
+// });
 
-server.listen(port, (err) => {
+const server = app.listen(port, (err) => {
   if (err) throw new Error(err);
 
   console.log(`Server Startes on Port ${port}`);
@@ -39,3 +40,26 @@ server.listen(port, (err) => {
 
 app.use(`/api/auth`, userRouter);
 app.use(`/api/messages`, messageRouter);
+
+//socket
+const io = socket(server, {
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on('connection', (socket) => {
+  global.chatSocket = socket;
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieve', data.message);
+    }
+  });
+});
